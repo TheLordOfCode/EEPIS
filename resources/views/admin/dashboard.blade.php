c4c8e6c64840e3ecf5779d9d40655fe2c727d5b8
Juan Guillermo Quevedo <jgqs1993@gmail.com>
2019-01-29 15:56:57 -0500

    dashboard iconos

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -46,12 +46,10 @@
                         </div>
                         <div class="span3">
                             <ul class="site-stats">
-                                <li class="bg_lh"><i class="icon-user"></i> <strong>2540</strong> <small>Total Users</small></li>
-                                <li class="bg_lh"><i class="icon-plus"></i> <strong>120</strong> <small>New Users </small></li>
-                                <li class="bg_lh"><i class="icon-shopping-cart"></i> <strong>656</strong> <small>Total Shop</small></li>
-                                <li class="bg_lh"><i class="icon-tag"></i> <strong>9540</strong> <small>Total Orders</small></li>
-                                <li class="bg_lh"><i class="icon-repeat"></i> <strong>10</strong> <small>Pending Orders</small></li>
-                                <li class="bg_lh"><i class="icon-globe"></i> <strong>8540</strong> <small>Online Orders</small></li>
+                                <li class="bg_lh"><i class="icon-user"></i> <strong>25</strong> <small>Estudiantes Evaluados</small></li>
+                                <li class="bg_lh"><i class="icon-plus"></i> <strong>10</strong> <small>Cuestionarios Creados </small></li>
+                                <li class="bg_lh"><i class="icon-warning-sign"></i> <strong>6</strong> <small>Estudiantes en posible riesgo</small></li>
+                                <li class="bg_lh"><i class="icon-bar-chart"></i> <strong>9</strong> <small>Variables Evaluadas</small></li>
                             </ul>
                         </div>
                     </div>
