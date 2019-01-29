b51a60bae135e14df6012a812eb986644f7b2851
Juan Guillermo Quevedo <jgqs1993@gmail.com>
2019-01-29 14:46:12 -0500

    formulario carga de datos de cuestionarios

--- resources/views/admin/upload_data.blade.php
+++ resources/views/admin/upload_data.blade.php
@@ -3,107 +3,38 @@
     <div id="content">
         <div id="content-header">
             <div id="breadcrumb"> <a href="{{ url('/home') }}" title="ir a Inicio" class="tip-bottom"><i class="icon-home"></i> Inicio</a> <a href="{{ url('/home/results') }}" title="ir a Cargar Datos" class="tip-bottom"><i class="icon icon-upload-alt"></i> Cargar Datos</a>  </div>
-            <h1>Form validation</h1>
+            <h1>Cargar datos de respuestas en cuestionarios</h1>
         </div>
         <div class="container-fluid"><hr>
+
             <div class="row-fluid">
                 <div class="span12">
                     <div class="widget-box">
-                        <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
-                            <h5>Form validation</h5>
-                        </div>
-                        <div class="widget-content nopadding">
-                            <form class="form-horizontal" method="post" action="#" name="basic_validate" id="basic_validate" novalidate="novalidate">
-                                <div class="control-group">
-                                    <label class="control-label">Your Name</label>
-                                    <div class="controls">
-                                        <input type="text" name="required" id="required">
-                                    </div>
-                                </div>
-                                <div class="control-group">
-                                    <label class="control-label">Your Email</label>
-                                    <div class="controls">
-                                        <input type="text" name="email" id="email">
-                                    </div>
-                                </div>
-                                <div class="control-group">
-                                    <label class="control-label">Date (only Number)</label>
-                                    <div class="controls">
-                                        <input type="text" name="date" id="date">
-                                    </div>
-                                </div>
-                                <div class="control-group">
-                                    <label class="control-label">URL (Start with http://)</label>
-                                    <div class="controls">
-                                        <input type="text" name="url" id="url">
-                                    </div>
-                                </div>
-                                <div class="form-actions">
-                                    <input type="submit" value="Validate" class="btn btn-success">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row-fluid">
-                <div class="span12">
-                    <div class="widget-box">
-                        <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
-                            <h5>Numeric validation</h5>
+                        <div class="widget-title"> <span class="icon"> <i class="icon-file"></i> </span>
+                            <h5>Cargar archivo CSV</h5>
                         </div>
                         <div class="widget-content nopadding">
                             <form class="form-horizontal" method="post" action="#" name="number_validate" id="number_validate" novalidate="novalidate">
                                 <div class="control-group">
-                                    <label class="control-label">Minimal Salary</label>
+                                    <label class="control-label">Seleccionar prueba</label>
                                     <div class="controls">
-                                        <input type="text" name="min" id="min" />
+                                        <select type="text" name="prueba" id="prueba" required="required">
+                                            <option value=""></option>
+                                            <option value="1">Prueba de motivación</option>
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="control-group">
-                                    <label class="control-label">Maximum Salary</label>
+                                    <label class="control-label">Archivo</label>
                                     <div class="controls">
-                                        <input type="text" name="max" id="max" />
+                                        <input type="file" name="archivo_csv" id="archivo_csv" />
                                     </div>
                                 </div>
-                                <div class="control-group">
-                                    <label class="control-label">Only digit</label>
-                                    <div class="controls">
-                                        <input type="text" name="number" id="number" />
-                                    </div>
-                                </div>
+
                                 <div class="form-actions">
-                                    <input type="submit" value="Validate" class="btn btn-success">
+                                    <button type="submit" class="btn btn-success"> Cargar </button>
                                 </div>
                             </form>
-                        </div>
-                    </div>
-                </div>
-                <div class="row-fluid">
-                    <div class="span12">
-                        <div class="widget-box">
-                            <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
-                                <h5>Security validation</h5>
-                            </div>
-                            <div class="widget-content nopadding">
-                                <form class="form-horizontal" method="post" action="#" name="password_validate" id="password_validate" novalidate="novalidate">
-                                    <div class="control-group">
-                                        <label class="control-label">Password</label>
-                                        <div class="controls">
-                                            <input type="password" name="pwd" id="pwd" />
-                                        </div>
-                                    </div>
-                                    <div class="control-group">
-                                        <label class="control-label">Confirm password</label>
-                                        <div class="controls">
-                                            <input type="password" name="pwd2" id="pwd2" />
-                                        </div>
-                                    </div>
-                                    <div class="form-actions">
-                                        <input type="submit" value="Validate" class="btn btn-success">
-                                    </div>
-                                </form>
-                            </div>
                         </div>
                     </div>
                 </div>
